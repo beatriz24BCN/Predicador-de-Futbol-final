3550cdd8f3aafdc1f927b36d651cfc8caf15dc8b
Alvaro Franco Abreu Forbidussi <136503161+aforbidussi@users.noreply.github.com>
2026-05-10 18:06:27 +0000

    finalización de descripción de portal y cards en Home

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Body } from "../components/Body.jsx";
 
 export const Home = () => {
 
@@ -34,19 +35,18 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
-		</div>
+            {Body}
+            <Body /> 
+
+            <div className="alert alert-info mt-4">
+                {store.message ? (
+                    <span>{store.message}</span>
+                ) : (
+                    <span className="text-danger">
+                        Conectando con el servidor de la liga... ⚽
+                    </span>
+                )}
+            </div>
+        </div>
 	);
 }; 
