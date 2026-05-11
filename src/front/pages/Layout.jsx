63d274bba86a16905de132f8c4d807c84128cce4
beatriz24BCN <beatriz24bcn@hotmail.com>
2026-05-11 18:53:56 +0000

    unificaccion de cambios

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,14 +1,11 @@
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Outlet } from "react-router-dom";
-import ScrollToTop from "../components/ScrollToTop";
-import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 
 export const Layout = () => {
     return (
         <ScrollToTop>
-            <Navbar />
             <div style={{ background: "#26753c", minHeight: "100vh" }}>
                 <Navbar />
                 <Outlet />
