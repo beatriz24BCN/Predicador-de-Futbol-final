4f2f14081670b5178994077a8a01ad66d7e2504e
Jose Daniel Tauil Wozniak <Danieltauil@hotmail.com>
2026-05-11 19:01:48 +0000

    camnbio Layout

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,14 +1,12 @@
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
+import { Footer} from "../components/Footer"
 import { Outlet } from "react-router-dom";
-import ScrollToTop from "../components/ScrollToTop";
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
+
 
 export const Layout = () => {
     return (
         <ScrollToTop>
-            <Navbar />
             <div style={{ background: "#26753c", minHeight: "100vh" }}>
                 <Navbar />
                 <Outlet />
