9a0364701bb82976058cd481d88f6f1240e526c8
GabyPinol <gabypinol435@gmail.com>
2026-05-12 16:38:08 +0000

    calendario creado

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,10 +1,13 @@
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
+import { Body } from "../components/Body"
+import { Home } from "./Home"
 
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
+            <Home />
         </ScrollToTop>
     )
 }
