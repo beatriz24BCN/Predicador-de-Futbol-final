51bbc4c38a6921af3ab4f965d4fb91724d6dca9b
beatriz24BCN <beatriz24bcn@hotmail.com>
2026-05-20 19:27:18 +0000

    añadiendo normas de usuario

---
+++ src/front/components/Normas.jsx
@@ -0,0 +1,14 @@
+export default function Normas() {
+  return (
+    <div className="normas-lateral">
+      <h3>📜 Normas</h3>
+
+      <ul>
+        <li>Respeta a todos</li>
+        <li>No insultos</li>
+        <li>No mensajes basura</li>
+        <li>Solo fútbol ⚽</li>
+      </ul>
+    </div>
+  );
+}
